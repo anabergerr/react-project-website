3d02dea76c06c8deece28f451a0e2b5b2f971df3
anabergerr <ana.berger@betrybe.com>
2022-04-30 00:46:59 -0300

    meu deus finalmente eu consegui arruma o particles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {/* <Background /> */}
+        <Background />
         <Infos />
       </main>
     </>
