5235340e10903dd7735814a12711744039f2e120
anabergerr <ana.berger@betrybe.com>
2022-05-07 22:42:55 -0300

    align items center

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -11,7 +11,6 @@ const Background = () => {
   };
   return (
     <>
-
       <Particles
         id="ts-particles"
         init={particlesInit}
--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -3,36 +3,37 @@ import Row from 'react-bootstrap/Row';
 
 
 const Infos = () => {
-    return (
-        <>
-            <Row className="mt-5">
-                <Col md={{ span: 4, offset: 4 }}>
-                    <h1>console.log('ana')</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ipsa molestias dolores delectus. Minima atque,
-                        error quas tenetur a itaque, odit magni, eum rem aliquam eius
-                        recusandae esse vero aut nesciunt.
-                    </p>
-                </Col>
-                <Col md={{ span: 4, offset: 4 }}>
-                    <h1>follow me, please :)</h1>
-                    <section>
-                        <a>
-                            <img src="imgs/linkedin.png" className='icon' />
-                            <a>
-                                <img src="imgs/github.png" className='icon' />
-                            </a>
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col md={{ span: 4, offset: 4 }}>
+          <h1>console.log('ana')</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ipsa molestias dolores delectus. Minima atque,
+            error quas tenetur a itaque, odit magni, eum rem aliquam eius
+            recusandae esse vero aut nesciunt.
+          </p>
+        </Col>
+        <Col md={{ span: 4, offset: 4 }}>
+          <h1>follow me, please :)</h1>
+          <section>
+            <a>
+              <img src="imgs/linkedin.png" className='icon' />
+              <a>
+                <img src="imgs/github.png" className='icon' />
+              </a>
 
-                        </a>
-                    </section>
+            </a>
+          </section>
 
-                </Col>
+        </Col>
 
-            </Row>
+      </Row>
 
-        </>
-    )
+
+    </>
+  )
 }
 
 export default Infos
